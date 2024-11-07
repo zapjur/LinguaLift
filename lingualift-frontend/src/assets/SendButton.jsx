a261d301176f4453fc7c7166b02710d01bc662f7
Piotr Zapiór <piotrzapior0@gmail.com>
2024-11-07 13:24:17 +0100

    finish

--- lingualift-frontend/src/assets/SendButton.jsx
+++ lingualift-frontend/src/assets/SendButton.jsx
@@ -25,64 +25,64 @@ const SendButton = ({transcript, language, onTranscriptChange}) => {
 
         onTranscriptChange('')
 
-        // try {
-        //     const res = await fetch('http://localhost:8080/input', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify(data),
-        //     });
-        //
-        //     if (!res.ok) {
-        //         throw new Error("API Error");
-        //     }
-        //
-        //     const jsonResponse = await res.json();
-        //     console.log(jsonResponse);
-        //     const userInput = jsonResponse.user_input;
-        //     const chatResponse = jsonResponse.chat_response;
-        //     const errors = jsonResponse.errors;
-        //     setUserInputs((prevUserInputs) => [...prevUserInputs, userInput]);
-        //     setChatResponses((prevChatResponses) => [
-        //                     ...prevChatResponses,
-        //                     chatResponse,
-        //     ]);
-        //     setAllErrors((prevErrors) => [...prevErrors, ...errors]);
-        // } catch (error) {
-        //     alert(error);
-        // } finally {
-        //     setLoading(false);
-        // }
+        try {
+            const res = await fetch('http://localhost:8080/input', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
 
-        //await delay(3000);
-        fetch('http://localhost:8000/input')
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                const userInput = data.user_input;
-                const chatResponse = data.chat_response;
-                const errors = data.errors;
+            if (!res.ok) {
+                throw new Error("API Error");
+            }
 
-                setUserInputs((prevUserInputs) => [...prevUserInputs, userInput]);
-                setChatResponses((prevChatResponses) => [
-                    ...prevChatResponses,
-                    chatResponse,
-                ]);
-                setAllErrors((prevErrors) => [...prevErrors, ...errors]);
+            const jsonResponse = await res.json();
+            console.log(jsonResponse);
+            const userInput = jsonResponse.user_input;
+            const chatResponse = jsonResponse.chat_response;
+            const errors = jsonResponse.errors;
+            setUserInputs((prevUserInputs) => [...prevUserInputs, userInput]);
+            setChatResponses((prevChatResponses) => [
+                            ...prevChatResponses,
+                            chatResponse,
+            ]);
+            setAllErrors((prevErrors) => [...prevErrors, ...errors]);
+        } catch (error) {
+            alert(error);
+        } finally {
+            setLoading(false);
+        }
 
-            })
-            .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-            })
-            .finally(() => {
-                setLoading(false);
-            } )
+        await delay(3000);
+    //     fetch('http://localhost:8000/input')
+    //         .then(response => {
+    //             if(!response.ok) {
+    //                 throw new Error('Network response was not ok');
+    //             }
+    //             return response.json();
+    //         })
+    //         .then(data => {
+    //             console.log(data);
+    //             const userInput = data.user_input;
+    //             const chatResponse = data.chat_response;
+    //             const errors = data.errors;
+    //
+    //             setUserInputs((prevUserInputs) => [...prevUserInputs, userInput]);
+    //             setChatResponses((prevChatResponses) => [
+    //                 ...prevChatResponses,
+    //                 chatResponse,
+    //             ]);
+    //             setAllErrors((prevErrors) => [...prevErrors, ...errors]);
+    //
+    //         })
+    //         .catch(error => {
+    //             console.error('There has been a problem with your fetch operation:', error);
+    //         })
+    //         .finally(() => {
+    //             setLoading(false);
+    //         } )
     };
 
     return (
